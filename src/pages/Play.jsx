7f7cc47a5566897ad8041bb49e5f71e27e37c154
OvOpenguin <rosebox14@gmail.com>
2025-08-14 21:38:19 +0800

    0814-更新play和上傳play相關圖片

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -2,21 +2,26 @@ import "../sass/play.scss"
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 
-import 剪影 from "../images/互動遊戲-圖片-花卉剪影.svg"
+import 剪影 from "../images/play/play-flowerBlack-1.webp"
+import 開始遊戲 from "../images/play/play-start.svg"
+import 結束遊戲 from "../images/play/play-stop.svg"
+import 背景 from "../images/play/play-bg-yg.svg"
+
 const Play = () => {
     return (
         <>
             <Nav></Nav>
             <div className='playWrap'>
                 {/* 說明遊戲 */}
-                <div className="playH2">看花影 猜花卉</div>
-
+                
                 <div className="playExplain">
 
+                    <div className="playH2">看花影 猜花卉</div>
+
                     {/* 為了標題定位使用給上方一個div */}
-                    <div className="upExplain">
+                    <div className="playRule">
                         {/* 上方說明 */}
-                        <div className="rules">
+                        <div className="rule">
                             {/* 說明遊戲規則 */}
                             <p>花的姿態千變萬化，有些光靠剪影也能辨認。
                                 <br />透過這個互動遊戲，挑戰你的植物觀察力！
@@ -26,29 +31,29 @@ const Play = () => {
                     </div>
 
                     {/* 下方開始/結束按鍵 */}
-                    <div className='startBtn'>
-                        <div className="btnS">開始遊戲</div>
-                        <div className="btnE">結束遊戲</div>
+                    <div className='playBtn'>
+                        <div c><img src={開始遊戲} alt="開始遊戲" /></div>
+                        <div><img src={結束遊戲} alt="結束遊戲" /></div>
                     </div>
-
                 </div>
 
                 {/* 遊戲圖片+選項 */}
                 <div className='questions'>
 
                     {/* 圖片載入 */}
+                    <div className="play-bg"><img src={背景} alt="背景" /></div>
                     <figure><img src={剪影} alt="花" /></figure>
 
                     {/* 圖片按鍵 */}
                     <div className='answer'>
                         {/* 選項項目 */}
-                        <div className='a1'>1</div>
-                        <div className='a2'>2</div>
-                        <div className='a3'>3</div>
+                        
+                        <div className='a1'>玫瑰</div>
+                        <div className='a2'>雛菊</div>
+                        <div className='a3'>百合</div>
                         {/* 下一題 */}
-                        <div>下一題</div>
+                        <div className="next">下一題</div>
                     </div>
-
                 </div>
             </div>
             <Footer></Footer>
