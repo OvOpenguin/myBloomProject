697de901a2809e42f5eab84703ff207af4d5572b
Sawmahmoli <sswmhh2022@gmail.com>
2025-09-02 15:53:47 +0800

    0902-member-login

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -22,6 +22,10 @@ function SidebarItem({ label, onClick, active = false, danger = false }) {
                         xmlns="http://www.w3.org/2000/svg" width="63" height="64" viewBox="0 0 63 64" fill="none">
                         <path d="M42.9996 1.23745C39.1717 0.541481 34.6914 0.62848 32.9514 0.62848C29.2106 0.584982 25.9047 1.36795 22.2943 2.45541C18.336 3.62986 11.1588 7.10973 7.8964 11.7206C4.63402 16.3314 2.80709 21.5077 2.02412 24.7266C1.54564 26.684 1.45863 28.6414 1.19764 30.6423C0.849653 33.7307 0.371187 40.299 1.89363 44.9533C3.41607 49.6076 6.46096 53.218 8.0704 54.3489C11.0718 56.3934 13.8557 58.3073 17.1181 59.8297C18.6405 60.5257 20.1629 61.3087 21.7289 61.8741C23.5993 62.5266 25.5567 62.8746 27.4707 63.3096C32.9515 64.5711 47.6974 61.5697 50.3073 60.3082C52.9172 59.0468 57.267 54.3924 58.224 52.5655C59.5724 49.9556 62.7043 37.4281 62.7478 35.9926C62.7913 33.7742 62.1823 24.6396 61.3994 22.3777C59.3549 16.5054 54.9181 10.0676 53.3522 8.1537C51.8297 6.19627 46.8709 1.93343 42.9996 1.23745Z" fill="#69C9A0" />
                     </svg>
+                    <svg className="arrow"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="11" viewBox="0 0 24 11" fill="none">
+                        <path d="M1.55664 9.61914H22.1281L14.1087 1.61914" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
 
                 </div>
             </button>
@@ -160,11 +164,17 @@ function Profile() {
 }
 
 // 登入畫面
-function SignIn() {
+function SignIn({onLogin}) {
     return (
         <div className="signin-wrap">
             <img src={logo} alt="" />
-            <form action="" method="post">
+            <form action="" method="post"
+
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    onLogin(); // 假設直接登入成功
+                }}
+            >
                 {/* 帳號 */}
                 <div className="name">
                     <label htmlFor="username">帳號</label>
@@ -198,6 +208,12 @@ function SignIn() {
 
 // 切換各分頁的陣列
 export default function MemberCenter() {
+
+    // 登入
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // 宣告變數 => 顯示目前分頁
+    const [activeKey, setActiveKey] = useState("logout");
+
     const TABS = [
         { key: "favorites", label: "我的收藏", view: <Favorites /> },
         { key: "wall", label: "我的花牆", view: <Wall /> },
@@ -206,33 +222,47 @@ export default function MemberCenter() {
         { key: "logout", label: "登出", danger: true },
     ];
 
-    // 宣告變數 => 顯示目前分頁
-    const [activeKey, setActiveKey] = useState("logout");
 
     // 將分頁設定為目前狀態
-    const handleSelect = (key) => { setActiveKey(key); };
+    const handleSelect = (key) => {
+        if (key === "logout") {
+            setIsLoggedIn(false); // 登出
+            setActiveKey("favorites"); // 重置 activeKey
+        } else {
+            setActiveKey(key);
+        }
+    };
 
     // 根據選擇狀態搜尋渲染分頁
-    const activeTab = TABS.find((t) => t.key === activeKey);
+    const activeTab = isLoggedIn
+        ? TABS.find((t) => t.key === activeKey)
+        : null;
 
     return (
         <section className="wrapper">
             {/* 左側選單 */}
             <div className="left-sidebar">
-                {TABS.map((t) => (
-                    <SidebarItem
-                        key={t.key}
-                        label={t.label}
-                        active={activeKey === t.key}
-                        danger={t.danger}
-                        onClick={() => handleSelect(t.key)}
-                    />
-                ))}
+                {isLoggedIn &&
+
+                    TABS.map((t) => (
+                        <SidebarItem
+                            key={t.key}
+                            label={t.label}
+                            active={activeKey === t.key}
+                            danger={t.danger}
+                            onClick={() => handleSelect(t.key)}
+                        />
+                    ))
+                }
             </div>
 
             {/* 右側面板 */}
             <div className="right-panel">
-                {activeTab?.view ?? <SignIn />}
+                {isLoggedIn ? (
+                    activeTab?.view
+                ) : (
+                    <SignIn onLogin={() => setIsLoggedIn(true)} />
+                )}
             </div>
         </section>
 
