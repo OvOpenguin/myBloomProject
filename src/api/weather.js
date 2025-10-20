3f52ed818a434099b903afd82deee98ca4735de0
weic777 <anna0826777@gmail.com>
2025-10-20 14:45:20 +0800

    1020更新

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,6 +1,6 @@
 export async function fetchWeather(city) {
   const apiKey = "CWA-CB70C905-2148-49DF-A0CD-017A0C1BE1E1";
-  const url = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${apiKey}&locationName=${city}`;
+  const url = ` https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${apiKey}&locationName=${city}`;
 
   try {
     const res = await fetch(url);
